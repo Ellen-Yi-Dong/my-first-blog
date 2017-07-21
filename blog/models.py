babb26b2a0c9226c744cd534feb64808ae80f710
Ellen-Yi-Dong <ellen.dong92@gmail.com>
2017-07-21 11:24:39 -0700

    additional files and changes for Heroku

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
@@ -11,10 +12,28 @@ class Post(models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
 
+
     def publish(self):
         self.published_date = timezone.now()
-        self.author()
         self.save()
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
